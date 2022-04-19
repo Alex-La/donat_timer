791840c220f1f33d00d90b8043b1cfc4fe65ad89
Alex Larionov <lars6162@gmail.com>
2022-04-19 13:03:22 +0300

    Init timer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import { useTimer } from "react-timer-hook";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const { hours, minutes, seconds } = useTimer({
+    expiryTimestamp: new Date(),
+  });
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ color: "orange" }}>
+          {hours}:{minutes}:{seconds}
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default App;
