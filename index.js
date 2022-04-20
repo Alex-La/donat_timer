6fe3e027e933824fa32dff71dc2216bd2b7e2d04
Alex Larionov <lars6162@gmail.com>
2022-04-20 15:11:21 +0300

    Ready for test

---
+++ index.js
@@ -0,0 +1,45 @@
+import "dotenv/config";
+import express from "express";
+import { createServer } from "http";
+import path from "path";
+import { Server as SocketServer } from "socket.io";
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.set("views", path.join("views"));
+app.set("view engine", "pug");
+
+app.get("/", (_, res) => {
+  res.render("index", { title: "Timer" });
+});
+
+app.get("/settings", (_, res) => {
+  res.render("settings", { title: "Settings" });
+});
+
+const server = createServer(app);
+const io = new SocketServer(server);
+
+io.on("connection", (socket) => {
+  socket.join("room");
+
+  socket.on("start", (time) => {
+    io.to("room").emit("start", time * 1000);
+  });
+
+  socket.on("increase", (time) => {
+    io.to("room").emit("increase", time * 1000);
+  });
+
+  socket.on("decrease", (time) => {
+    io.to("room").emit("decrease", time * 1000);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected!");
+  });
+});
+
+server.listen(port, () => console.log(`Server ready at port: ${port}`));
