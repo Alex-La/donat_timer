ed7f703cdd26eeba3606649d5ddff53551a54256
Alex Larionov <lars6162@gmail.com>
2022-04-21 21:59:36 +0300

    Revert "Revert "Save session state for timer""
    This reverts commit 649142cf1709b6945ddbf545ead09145cb4042c9.

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,64 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
-  let timer,
-    date = new Date();
+  let date = new Date();
+  const timerObject = { timer: undefined };
+
+  socket.on("lastSessionTime", (lastDate) => {
+    date = new Date(lastDate);
+    startTimer(date, timerObject);
+  });
 
   socket.on("start", (time) => {
     date = new Date(new Date().getTime() + parseInt(time, 10));
-    startTimer(date);
+    startTimer(date, timerObject);
   });
 
   socket.on("increase", (time) => {
     date = new Date(date.getTime() + parseInt(time, 10));
-    startTimer(date);
+    startTimer(date, timerObject);
   });
 
   socket.on("decrease", (time) => {
     date = new Date(date.getTime() - parseInt(time, 10));
-    startTimer(date);
+    startTimer(date, timerObject);
   });
 
-  function startTimer(date) {
-    clearInterval(timer);
-    countdown(date);
+  window.onbeforeunload = () => {
+    socket.emit("disconnection", date);
+  };
+});
+
+function startTimer(date, timerObject) {
+  clearInterval(timerObject.timer);
+  countdown(date, timerObject);
+}
+
+//Timer logic
+function countdown(dateEnd, timerObject) {
+  hours, minutes, seconds;
+
+  dateEnd = dateEnd.getTime();
+
+  if (isNaN(dateEnd)) {
+    return;
   }
 
-  function countdown(dateEnd) {
-    let days, hours, minutes, seconds;
+  timerObject.timer = setInterval(calculate, 1000);
 
-    dateEnd = dateEnd.getTime();
+  function calculate() {
+    var dateStart = new Date();
 
-    if (isNaN(dateEnd)) {
+    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
+
+    if (timeRemaining >= 0) {
+      hours = parseInt(timeRemaining / 3600);
+      timeRemaining = timeRemaining % 3600;
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = timeRemaining % 60;
+      seconds = parseInt(timeRemaining);
+
+      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
       return;
     }
-
-    timer = setInterval(calculate, 1000);
-
-    function calculate() {
-      var dateStart = new Date();
-
-      var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
-
-      if (timeRemaining >= 0) {
-        days = parseInt(timeRemaining / 86400);
-        timeRemaining = timeRemaining % 86400;
-        hours = parseInt(timeRemaining / 3600);
-        timeRemaining = timeRemaining % 3600;
-        minutes = parseInt(timeRemaining / 60);
-        timeRemaining = timeRemaining % 60;
-        seconds = parseInt(timeRemaining);
-
-        document.getElementById("days").innerHTML = parseInt(days, 10);
-        document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-        document.getElementById("minutes").innerHTML = ("0" + minutes).slice(
-          -2
-        );
-        document.getElementById("seconds").innerHTML = ("0" + seconds).slice(
-          -2
-        );
-      } else {
-        return;
-      }
-    }
   }
-});
+}
