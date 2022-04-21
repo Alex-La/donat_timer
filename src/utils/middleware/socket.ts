92bba686e42f6f46976ebfd0b3ef5c5ea5b83994
Alex Larionov <lars6162@gmail.com>
2022-04-21 21:59:26 +0300

    Revert "Merge branch 'master' of https://github.com/Alex-La/bright_timer"
    This reverts commit 0630fcc59f097c7d502c771fdd19a370ebfe2c86, reversing
    changes made to 649142cf1709b6945ddbf545ead09145cb4042c9.

--- src/utils/middleware/socket.ts
+++
@@ -1,7 +0,0 @@
-export const wrap = (middleware) => (socket, next) =>
-  middleware(socket.request, {}, next);
-
-export const isAuth = (socket, next) => {
-  if (socket.request.session.passport?.user) next();
-  else next(new Error("unauthorized"));
-};
