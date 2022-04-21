49b9fb9e2421432c88f1cd17b6668528382883e3
Alex Larionov <lars6162@gmail.com>
2022-04-21 23:28:03 +0300

    Cookies trubbles

--- src/utils/middleware/session.ts
+++ src/utils/middleware/session.ts
@@ -2,13 +2,13 @@ import session from "express-session";
 
 const SESSION_SECRET = process.env.SESSION_SECRET!!;
 const COOKIE_MAX_AGE = parseInt(process.env.COOKIE_MAX_AGE!!, 10);
-const isDev = process.env.NODE_ENV === "development";
+const isProd = process.env.NODE_ENV === "production";
 
 const sessionMiddleware = session({
   secret: SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: !isDev, maxAge: COOKIE_MAX_AGE * 1000 },
+  cookie: { secure: isProd, maxAge: COOKIE_MAX_AGE * 1000 },
 });
 
 export default sessionMiddleware;
