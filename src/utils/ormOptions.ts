2740feec8a072a208ca0dc646b0e672e1aa82d04
Alex Larionov <lars6162@gmail.com>
2022-04-21 11:45:09 +0300

    Update ormOptions.ts

--- src/utils/ormOptions.ts
+++ src/utils/ormOptions.ts
@@ -1,11 +1,14 @@
 import { DataSourceOptions } from "typeorm";
 import models from "../models/idnex";
 
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
 const isDev = process.env.NODE_ENV === "development";
 
 const ormOptions = (url: string): DataSourceOptions => ({
   type: "postgres",
   url,
+  ssl: true,
   synchronize: true,
   logging: false,
   entities: models,
