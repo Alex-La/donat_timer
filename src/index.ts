9eb148cbf0062ba86ab047e974d7d399788a2f2a
Alex Larionov <lars6162@gmail.com>
2022-04-21 17:46:25 +0300

    Save session state for timer
    Auth middleware for socket.io
    
    Update timer

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { createServer } from "http";
 import path from "path";
 import { Server as SocketServer } from "socket.io";
 import { initSockets } from "./socket";
-import session from "express-session";
+import { SoketServer as SocketServerType } from "./socket/types";
 import flash from "connect-flash";
 import passport from "passport";
 import localStrategy from "./utils/passport/localStrategy";
@@ -14,14 +14,17 @@ import privateRouter from "./routes/private";
 import { DataSource } from "typeorm";
 import ormOptions from "./utils/ormOptions";
 import Store from "./utils/store";
-import { User } from "./models/entities/UserEntity";
+import sessionMiddleware from "./utils/middleware/session";
+import { wrap, isAuth as isAuthIO } from "./utils/middleware/socket";
+import UserEntity from "./models/entities/UserEntity";
 
 const PORT = process.env.PORT!!;
-const SESSION_SECRET = process.env.SESSION_SECRET!!;
 const DATABASE_URL = process.env.DATABASE_URL!!;
 
-passport.serializeUser<User>((user, done) => done(null, user as User));
-passport.deserializeUser<User>((user, done) => done(null, user));
+passport.serializeUser<UserEntity>((user, done) =>
+  done(null, user as UserEntity)
+);
+passport.deserializeUser<UserEntity>((user, done) => done(null, user));
 
 const dataSource = new DataSource(ormOptions(DATABASE_URL));
 
@@ -34,13 +37,7 @@ dataSource
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use(
-      session({
-        secret: SESSION_SECRET,
-        resave: true,
-        saveUninitialized: false,
-      })
-    );
+    app.use(sessionMiddleware);
     app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
@@ -61,7 +58,11 @@ dataSource
     app.get("*", (_, res) => res.render("404"));
 
     const server = createServer(app);
-    const io = new SocketServer(server);
+    const io: SocketServerType = new SocketServer(server);
+
+    io.use(wrap(sessionMiddleware));
+    io.use(isAuthIO);
+
     initSockets(io);
 
     server.listen(PORT, () => console.log(`Server ready at port: ${PORT}`));
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,19 +1,31 @@
-import { Server } from "socket.io";
+//@ts-nocheck
+import UserEntity from "../models/entities/UserEntity";
+import { SoketServer } from "./types";
 
-export const initSockets = (io: Server) => {
+export const initSockets = (io: SoketServer) => {
   io.on("connection", (socket) => {
-    socket.join("room");
+    const user = socket.request.session.passport.user as UserEntity;
+
+    socket.join(user.id);
+
+    if (user.lastTimerDate)
+      io.to(user.id).emit("lastSessionTime", user.lastTimerDate);
 
     socket.on("start", (time) => {
-      io.to("room").emit("start", time * 1000);
+      io.to(user.id).emit("start", time * 1000);
     });
 
     socket.on("increase", (time) => {
-      io.to("room").emit("increase", time * 1000);
+      io.to(user.id).emit("increase", time * 1000);
     });
 
     socket.on("decrease", (time) => {
-      io.to("room").emit("decrease", time * 1000);
+      io.to(user.id).emit("decrease", time * 1000);
+    });
+
+    socket.on("disconnection", (date) => {
+      socket.request.session.passport.user.lastTimerDate = date;
+      socket.request.session.save();
     });
 
     socket.on("disconnect", () => {
